a439c1812a1c94fe43519c6a8505793370b208d7
5910110296 <5910110296@psu.ac.th>
2018-09-21 19:36:03 +0700

    Styling & More

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ const word = "Hello";
 class App extends Component {
   render() {
     return (
-        <div>
-         {
-            Array.from(word).map((c , i) => <CharacterCard value ={c} key = {i}/>)
-         }
-        </div>
-      );
+      <div className="">{
+        Array.from(word).map((c, i) => 
+          < CharacterCard value={c} key={i} />)
+        }
+      </div>
+    );
   }
 }
 export default App;
